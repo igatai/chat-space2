46ecb62d823873fcadc5b71e6960de0b2ec4ce63
igatai <taiti0614@gmail.com>
2020-04-09 19:44:19 +0900

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # devise_controller呼び出しにより、 :configure_permitted_parametersが実行される。
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name] )
+  end
 end
